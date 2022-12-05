2a327d1a8e00b594927e2fd4e0baa1dc7e73bb31
ci-gitlab <tds-developer@xd.com>
2022-12-05 19:55:28 +0800

    feat:update upm

---
+++ Plugins/iOS/TapBillboardSDK.framework/Headers/TapBillboardSDK.h
@@ -0,0 +1,11 @@
+//
+//  TapBillboardSDK.h
+//  TapBillboardSDK
+//
+//  Created by TapTap on 2022/7/18.
+//
+
+#import <Foundation/Foundation.h>
+
+#import <TapBillboardSDK/TapBillboard.h>
+#import <TapBillboardSDK/BadgeDetails.h>
